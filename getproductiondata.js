2eba367f8ec408bfcbbb6b00493f3062e7a258e8
Muhammad Nadeem Khan smws <naseemsmws@gmail.com>
2025-12-16 16:21:48 +0500

    again commit with new changes

--- getproductiondata.js
+++ getproductiondata.js
@@ -1,7 +1,36 @@
 const mainContainerEl = document.querySelector(".main-container");
+const tableHeadEl = document.querySelector(".tbl-head-EL");
+const tableBodyEl = document.querySelector(".tbl-body-EL");
 
 let proDtl = JSON.parse(localStorage.getItem("productionDetails")) || [];
-console.log(Object.keys(proDtl[0]));
+
+const rowHeadEl = document.createElement("tr");
+tableHeadEl.appendChild(rowHeadEl);
+
 Object.keys(proDtl[0]).forEach(headers => {
     console.log(headers.toLocaleUpperCase());
-})
+    const thEl = document.createElement("th");
+    thEl.innerText = headers.toLocaleUpperCase();
+    rowHeadEl.appendChild(thEl);    
+});
+
+const twenty20 = proDtl.slice(28,35);
+
+twenty20.forEach((proDtl, index) => {
+    const dataRowEl = document.createElement("tr");
+    tableBodyEl.appendChild(dataRowEl);
+
+    const qcNumTdEl = document.createElement("td");
+    const sttNumTdEl = document.createElement("td");
+    const productNmTdEl = document.createElement("td");
+    const numPackTdEl = document.createElement("td");
+    const numPiecesTdEl = document.createElement("td");
+
+    qcNumTdEl.innerText = proDtl.qcNo;
+    sttNumTdEl.innerText = proDtl.sttNo;
+    productNmTdEl.innerText = (index+1) + "-" + proDtl.productName;
+    numPackTdEl.innerText = proDtl.numPack;
+    numPiecesTdEl.innerText = proDtl.numPieces;
+
+    dataRowEl.append(qcNumTdEl, sttNumTdEl, productNmTdEl, numPackTdEl, numPiecesTdEl);
+});
